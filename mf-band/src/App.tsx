dda68de185b0c8b6255161e8c2c0adffcafb80d3
Ivan Junior <contato.ivanfrancajr@gmail.com>
2024-03-20 12:01:16 -0300

    feat: add dark color variant

--- mf-band/src/App.tsx
+++ mf-band/src/App.tsx
@@ -1,10 +1,13 @@
+import { lazy } from "react";
+
 import Header from "./components/Header/Header";
-import Headliners from "./components/Headliners/Headliners";
+
 import Hero from "./components/Hero/Hero";
 
+const Headliners = lazy(() => import("./components/Headliners/Headliners"));
 function App() {
   return (
-    <main>
+    <main className="bg-zinc-50 dark:bg-zinc-900">
       <Header />
       <Hero />
       <Headliners />
--- mf-band/src/components/Headliners/Headliners.tsx
+++ mf-band/src/components/Headliners/Headliners.tsx
@@ -1,20 +1,20 @@
-import  { useState } from "react";
+import { useState } from "react";
 import { ShowList, HeadLiner } from "../../utils/data/ShowList";
 import HeadlinerCard from "./HeadlinerCard";
 
 const Headliners = () => {
-  const [showList, setShowList] = useState<HeadLiner[]>(ShowList);
+  const [artists, setArtists] = useState<HeadLiner[]>(ShowList);
 
   return (
-    <section className="max-w-screen-lg mx-auto my-20">
+    <section className="max-w-screen-lg mx-auto px-10 mt-20 text-zinc-900 dark:text-zinc-200">
       <h2 className="">Headliners</h2>
       <p className="my-5">
-      Experience the sonic delight from these made-up artists across 7 days
-          that music fans are calling “one of the many gigs that will happen
-          this year!
+        Experience the sonic delight from these made-up artists across 7 days
+        that music fans are calling “one of the many gigs that will happen this
+        year!
       </p>
       <div className="mt-4 flex w-full pb-5 snap-x overflow-x-auto gap-4">
-        {showList.map((artist) => (
+        {artists.map((artist) => (
           <HeadlinerCard
             key={artist.artist}
             artist={artist.artist}
