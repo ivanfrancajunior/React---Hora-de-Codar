28cec75a5a331ae199006af9cb63725c9bf9adc9
Ivan Junior <contato.ivanfrancajr@gmail.com>
2024-03-21 10:34:17 -0300

    first commit

---
+++ state-manegement/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="flex w-full items-start justify-center mt-10 h-screen">
+      <h2 className="text-3xl font-bold">State manegement </h2>
+    </main>
+  );
+}
+
+export default App;
