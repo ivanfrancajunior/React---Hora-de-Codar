740634f5eff1d044624fe7d6a90abf375180fb9f
Ivan Junior <contato.ivanfrancajr@gmail.com>
2024-03-24 11:04:49 -0300

    feat: add useFetchDocuments hook

---
+++ 8.2 -project - miniblog/src/hooks/useFetchDocuments.jsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from "react";
+import { db } from "../firebase/config";
+import {
+  collection,
+  query,
+  orderBy,
+  onSnapshot,
+  where,
+} from "firebase/firestore";
+
+export const useFetchDocuments = (docCollection, search = null, uid = null) => {
+  const [documents, setDocuments] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  // deal with memory leak
+  const [isCancelled, setIsCancelled] = useState(false);
+
+  useEffect(() => {
+    async function loadData() {
+      if (isCancelled) {
+        return;
+      }
+
+      setLoading(true);
+
+      const collectionRef = await collection(db, docCollection);
+
+      try {
+        let q; //query 
+
+        if (search) {
+          q = await query(
+            collectionRef,
+            where("tags", "array-contains", search),
+            orderBy("createdAt", "desc")
+          );
+        } else if (uid) {
+          q = await query(
+            collectionRef,
+            where("uid", "==", uid),
+            orderBy("createdAt", "desc")
+          );
+        } else {
+          q = await query(collectionRef, orderBy("createdAt", "desc"));
+        }
+
+        await onSnapshot(q, (querySnapshot) => {
+          setDocuments(
+            querySnapshot.docs.map((doc) => ({
+              id: doc.id,
+              ...doc.data(),
+            }))
+          );
+        });
+      } catch (error) {
+        console.log(error);
+        setError(error.message);
+      }
+
+      setLoading(false);
+    }
+
+    loadData();
+  }, [docCollection, search, uid, isCancelled]);
+
+  console.log(documents);
+
+  useEffect(() => {
+    return () => setIsCancelled(true);
+  }, []);
+
+  return { documents, loading, error };
+};
