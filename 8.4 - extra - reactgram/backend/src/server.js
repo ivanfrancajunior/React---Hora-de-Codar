888b7c556f4120915234271cd421753c29305cda
Ivan Junior <contato.ivanfrancajr@gmail.com>
2024-04-12 17:08:56 -0300

    fix fix wrong config

--- 8.4 - extra - reactgram/backend/src/server.js
+++ 8.4 - extra - reactgram/backend/src/server.js
@@ -1,20 +1,35 @@
 require("dotenv").config();
-require("./config/db.js");
+
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
 
+const PORT = process.env.PORT;
+
 const app = express();
+
+// Config JSON and form data response
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Solve CORS
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+
+// Upload directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+// db connection
+require("./config/db.js");
+
+// test route
+app.get("/", (req, res) => {
+  res.send("API Working!");
+});
+
+// routes
 const router = require("./routes/Router.js");
 
 app.use(router);
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
-
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-
-const PORT = process.env.PORT || 3077;
 
 app.listen(PORT, async () => {
   
