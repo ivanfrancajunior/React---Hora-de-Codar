03c3768a3a4808209a0e463d993c722264512792
Ivan Junior <contato.ivanfrancajr@gmail.com>
2024-04-13 17:38:34 -0300

    feat: add css base to Navbar + Footer components

---
+++ 8.4 - extra - reactgram/frontend/src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import "./Navbar.css";
+import {
+  BsSearch,
+  BsHouseDoorFill,
+  BsFillPersonFill,
+  BsFillCameraFill,
+} from "react-icons/bs";
+import { Link, NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav id="nav">
+      <Link to="/">ReactGram</Link>
+      <form>
+        <BsSearch />
+        <input type="text" placeholder="Enconte algo..." />
+      </form>
+      <ul id="nav-links">
+        <li>
+          <NavLink to="/">
+            <BsHouseDoorFill />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/register">Cadastrar</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login">Entrar</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
