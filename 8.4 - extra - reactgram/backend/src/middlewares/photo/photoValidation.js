99dc5a837405e4c15cd2e0f0c54e391ed23af94d
Ivan Junior <contato.ivanfrancajr@gmail.com>
2024-04-13 00:44:25 -0300

    feat: add photoInsertValidation

---
+++ 8.4 - extra - reactgram/backend/src/middlewares/photo/photoValidation.js
@@ -0,0 +1,22 @@
+const { body } = require("express-validator");
+
+const photoInsertValidation = () => {
+  return [
+    body("title")
+      .not()
+      .equals("undefined")
+      .withMessage("Title is required.")
+      .isString()
+      .withMessage("Title is required.")
+      .isLength({ min: 3 })
+      .withMessage("The title must be at least 3 characters long."),
+    body("image").custom((value, { req }) => {
+      if (!req.file) {
+        throw new Error("Image is required");
+      }
+      return true;
+    }),
+  ];
+};
+
+module.exports = { photoInsertValidation };
