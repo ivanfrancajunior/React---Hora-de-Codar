528dd35b024d4df61fe3dfb5f033dc07fa154efa
Ivan Junior <contato.ivanfrancajr@gmail.com>
2024-04-12 21:53:38 -0300

    feat: add getCurrentUser funtion to userController

--- 8.4 - extra - reactgram/backend/src/routes/UserRoutes.js
+++ 8.4 - extra - reactgram/backend/src/routes/UserRoutes.js
@@ -2,10 +2,20 @@ const express = require("express");
 const router = express.Router();
 
 const validate = require("../middlewares/handleValidations.js");
-const { userCreateValidation, loginValidation } = require("../middlewares/userValidations.js");
-const { register, login } = require("../controllers/userController");
+const {
+  userCreateValidation,
+  loginValidation,
+} = require("../middlewares/userValidations.js");
+
+const { authGuard } = require("../middlewares/authGuard.js");
+const {
+  register,
+  login,
+  getCurrentUser,
+} = require("../controllers/userController");
 
 router.post("/register", userCreateValidation(), validate, register);
 router.post("/login", loginValidation(), validate, login);
+router.get("/profile", authGuard, getCurrentUser);
 
 module.exports = router;
