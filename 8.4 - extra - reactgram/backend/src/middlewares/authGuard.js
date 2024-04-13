56f502ab18de57920d0e83124b351a1475c0787b
Ivan Junior <contato.ivanfrancajr@gmail.com>
2024-04-12 21:44:33 -0300

    feat: add auth middleware

---
+++ 8.4 - extra - reactgram/backend/src/middlewares/authGuard.js
@@ -0,0 +1,23 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const jwt_secret = process.env.JWT_SECRET;
+
+const authGuard = async (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) return res.status(401).json({ errors: ["Not authorized"] });
+
+  try {
+    const is_valid = jwt.verify(token, jwt_secret);
+
+    req.user = await User.findById(is_valid.id).select("-password");
+
+    next();
+  } catch (error) {
+    res.status(401).json({ errors: ["Invalid token."] });
+  }
+};
+
+module.exports = { authGuard };
