05f309af5a2395e2bf9ad9a4418d2c6c6069641f
Ivan Junior <contato.ivanfrancajr@gmail.com>
2024-04-14 09:54:50 -0300

    feat: add register page

--- 8.4 - extra - reactgram/frontend/src/pages/Auth/Register.jsx
+++ 8.4 - extra - reactgram/frontend/src/pages/Auth/Register.jsx
@@ -1,7 +1,27 @@
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import "./Auth.css";
 
 const Register = () => {
-  return <div>Register</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div id="register">
+      <h2>ReactGram</h2>
+      <p className="subtitle"> Cadastra-se para ver as fotos dos seus amgos</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome" />
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+        <input type="password" placeholder="Confirme a senha" />
+        <input type="submit" value={"Cadastrar"} />
+      </form>
+      <p>
+        Já possui conta? <Link to={"/login"}>Clique aqui</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Register;
