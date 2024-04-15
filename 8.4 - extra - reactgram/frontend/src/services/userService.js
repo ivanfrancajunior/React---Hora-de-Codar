aec8dd3fa1a42c38ad48d1abfb715579f090c58c
Ivan Junior <contato.ivanfrancajr@gmail.com>
2024-04-15 12:05:39 -0300

    feat: add getUserProfile service

---
+++ 8.4 - extra - reactgram/frontend/src/services/userService.js
@@ -0,0 +1,18 @@
+import { api_url, requestConfig } from '../utils/config';
+
+const getUserProfile = async (data, token) => {
+  const config = requestConfig('GET', data, token);
+
+  try {
+    const res = await fetch(api_url + 'users/profile', config)
+      .then((res) => res.json())
+      .catch((err) => err);
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const userService = { getUserProfile };
+
+export default userService;
