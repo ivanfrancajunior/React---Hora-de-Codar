72bf7f36751ed31199c7607269c5959115663c82
Ivan Junior <contato.ivanfrancajr@gmail.com>
2024-04-15 11:44:48 -0300

    add userSlice

---
+++ 8.4 - extra - reactgram/frontend/src/slices/userSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  user: {},
+  error: false,
+  success: false,
+  loading: false,
+  message: null,
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    resetMessage: (state) => (state.message = null),
+  },
+});
+
+export const { resetMessage } = userSlice.actions;
+
+export default userSlice.reducer;
