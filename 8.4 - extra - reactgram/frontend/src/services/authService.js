3c992ef0fa8e1b6a54f92f44039f3c115ff051be
Ivan Junior <contato.ivanfrancajr@gmail.com>
2024-04-15 23:06:45 -0300

    fix: fix wrong setted user to localstorage

--- 8.4 - extra - reactgram/frontend/src/services/authService.js
+++ 8.4 - extra - reactgram/frontend/src/services/authService.js
@@ -29,7 +29,7 @@ const login = async (data) => {
       .catch((err) => err);
 
       if (res._id) {
-      localStorage.setItem('user', JSON.stringify('user'));
+      localStorage.setItem('user', JSON.stringify(res));
     }
 
     return res;
