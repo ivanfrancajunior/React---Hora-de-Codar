8712ae3be35c0b79b3b80533957742c1b87611e5
Ivan Junior <contato.ivanfrancajr@gmail.com>
2024-04-16 09:36:31 -0300

    chore:add publishPhoto function

--- 8.4 - extra - reactgram/frontend/src/services/photoService.js
+++ 8.4 - extra - reactgram/frontend/src/services/photoService.js
@@ -1,5 +1,19 @@
 import { api_url, requestConfig } from '../utils/config';
 
-const photoService = {};
+
+const publishPhoto = async (data, token) => {
+  const config = requestConfig('POST', data, token, true);
+
+  try {
+    const res = await fetch(api_url + '/photos', config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+const photoService = { publishPhoto };
 
 export default photoService;
