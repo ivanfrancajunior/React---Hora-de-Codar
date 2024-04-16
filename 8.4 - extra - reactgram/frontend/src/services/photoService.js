240286c17c3d49203a2a423b2aaf5ba63aa59b31
Ivan Junior <contato.ivanfrancajr@gmail.com>
2024-04-16 17:31:28 -0300

    feat: add likePhoto function

--- 8.4 - extra - reactgram/frontend/src/services/photoService.js
+++ 8.4 - extra - reactgram/frontend/src/services/photoService.js
@@ -70,12 +70,26 @@ const getPhotoById = async (id, token) => {
   }
 };
 
+const likePhoto = async (id, token) => {
+  const config = requestConfig('PUT', null, token);
+
+  try {
+    const res = await fetch(api_url + 'photo/like' + id)
+      .then((res) => res.json())
+      .catch((err) => err);
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const photoService = {
   publishPhoto,
   getUserPhotos,
   deletePhoto,
   updatePhoto,
   getPhotoById,
+  likePhoto,
 };
 
 export default photoService;
