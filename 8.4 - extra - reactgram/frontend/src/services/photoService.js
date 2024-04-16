16cf7950852151048b29ac0dd27e24621d04de99
Ivan Junior <contato.ivanfrancajr@gmail.com>
2024-04-16 12:54:30 -0300

    feat: add updatePhoto function

--- 8.4 - extra - reactgram/frontend/src/services/photoService.js
+++ 8.4 - extra - reactgram/frontend/src/services/photoService.js
@@ -43,6 +43,19 @@ const deletePhoto = async (id, token) => {
   }
 };
 
-const photoService = { publishPhoto, getUserPhotos, deletePhoto };
+const updatePhoto = async (data, id, token) => {
+  const config = requestConfig('PUT', data, token);
+
+  try {
+    const res = await fetch(`${api_url}/photos/${id}`, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const photoService = { publishPhoto, getUserPhotos, deletePhoto, updatePhoto };
 
 export default photoService;
